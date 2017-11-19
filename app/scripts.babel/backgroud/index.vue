44b878cdfe7eda6720cbaee0560f9f4229fa4f94
thandonguocmo2020 <thandonguocmo2020@gmail.com>
2017-11-19 09:54:41 +0700

    ok

--- app/scripts.babel/backgroud/index.vue
+++ app/scripts.babel/backgroud/index.vue
@@ -5,73 +5,107 @@
       <!-- ACTIOn -->
       <div class="card">
             <div class="card-header bg-danger">
-            Extension Get Link Follow
+            Extension Get Link Follow  
+        
+                     <button v-if="auth == '2'" type="button" class="mr-10 float-right"  @click="logout" name="logout">Logout</button>
+                
             </div>
             <div class="card-block">
-              <div class="row">
-              <div class="col-md-6">
 
-                   <form @submit.prevent class="form-inline">
-                
-                <div class="form-group mx-sm-3">
-                  <label for="kwd" class="sr-only">Keyword</label>
-                  <input type="text" class="form-control" name="kwd" id="search" placeholder="Tu Khoa">
+              <div class="row" v-if="auth != '2'">
+                <div class="col-md-6">
+                  <form v-on:submit.prevent="login($event)" class="form-inline">
+                      
+                      <div class="form-group mx-sm-3">
+                        <label for="kwd" class="sr-only">Đăng Nhập</label>
+                        <input type="email" class="form-control" name="uid" id="mail" placeholder="Mail">
+                      </div>
+                        <div class="form-group mx-sm-3">
+                        <label for="kwd" class="sr-only">Password</label>
+                        <input type="password" class="form-control" name="pwd" id="pass"  placeholder="Nhập mật khẩu">
+                      </div>
+
+                      <button type="submit" class="btn btn-primary btn-sm mr-10"  name="submit">Login</button>
+                    </form>
                 </div>
-                  <div class="form-group mx-sm-3">
+                <div class="col-md-6">
 
-                  <select class="custom-select mb-2 mr-sm-2 mb-sm-0" id="type">
-                  <option selected value="people">Theo loại...</option>
-                  <option value="boards">boards</option>
-                  <option value="people">people</option>
-                  <option value="pins">pins</option>
-                </select>
+                    <div>{{ msgLogin }}</div>
 
                 </div>
-
-                <button type="button" class="btn btn-primary btn-sm mr-10"  @click="search" name="submit">Tìm Kiếm</button>
-            
-              </form>
               </div>
-      
-              <div class="col-md-6"> 
-                  <button type="button" class="btn btn-success btn-sm mr-10"  @click="scanner" name="scanner">Quét Link Này</button>
-                  <button type="button" class="btn btn-warning btn-sm mr-10"  @click="stopScanner" name="stop-scanner">Dừng Quét</button>
+        
+
+              <div class="row" v-if="auth == '2'">
+                <div class="col-md-6">
+
                
-                  <button type="button" class="btn btn-info btn-sm mr-10 pull-right"  @click="sendLink" name="stop-scanner">Lưu Dữ Liệu</button>
-              
-                </div>
-              </div>
-          </div>
-      </div>
-      <!-- END ACTIOn -->
+                  <form @submit.prevent class="form-inline">
+                  
 
-      <!-- DATA -->
-            <table class="table table-responsive">
+                  <div class="form-group mx-sm-3">
+                    <label for="kwd" class="sr-only">Keyword</label>
+                    <input type="text" class="form-control" name="kwd" id="search" placeholder="Tu Khoa">
+                  </div>
+                    <div class="form-group mx-sm-3">
+
+                    <select class="custom-select mb-2 mr-sm-2 mb-sm-0" id="type">
+                    <option selected value="people">Theo loại...</option>
+                    <option value="boards">boards</option>
+                    <option value="people">people</option>
+                    <option value="pins">pins</option>
+                  </select>
+
+                  </div>
+
+                  <button type="button" class="btn btn-primary btn-sm mr-10"  @click="search" name="submit">Tìm Kiếm</button>
+              
+                </form>
+                </div>
+        
+                <div class="col-md-6"> 
+                    <button type="button" class="btn btn-success btn-sm mr-10"  @click="scanner" name="scanner">Quét Link Này</button>
+                    <button type="button" class="btn btn-warning btn-sm mr-10"  @click="stopScanner" name="stop-scanner">Dừng Quét</button>
+                
+                    <button type="button" class="btn btn-info btn-sm mr-10 pull-right"  @click="sendLink" name="stop-scanner">Lưu Dữ Liệu</button>
+                
+                  </div>
+                </div>
+             </div>
+            <!-- DATA -->
+            <table class="table table-responsive" v-if="auth == '2'">  
                 <!-- On rows -->
             
-            <thead>
+             <thead>
                 <tr class="table-info">
-                  
+                  <th>Kết Quả</th>  
                   <th>Số lần tải DOM</th>
                   <th>Số link đã nạp</th>
-                  <th>Số lần reset load</th>
+                
                   <th>Trạng Thái</th>
                   <th>Thời gian quét </th>
-                  <th>Kết Quả</th>
+                  <th>Số lần reset load</th>
                 </tr>
               </thead>
     
               <tr class="bg-warning">
+                <td>{{result.length}} link</td>
                 <td>{{ n }}</td>
                 <td>{{links.length}}</td>
-                <td>{{count }}</td>
                 <td>{{ status }}</td>
                 <td>{{timeout}} ms</td>
-                <td>{{result.length}} link</td>
+                <td>{{count }}</td>
               </tr>
       
             </table>
+        
+  
+          
       <!-- DATA -->
+      </div>
+      <!-- END ACTIOn -->
+
+    
   </div>
 </div>
 </template>
@@ -87,6 +121,9 @@ import moment from "moment";
 export default {
   data() {
     return {
+      auth: localStorage.getItem("auth"), // string
+      msgLogin: "",
+      uid: localStorage.getItem("uid"),
       links: [],
       continue: 0,
       stop: 0,
@@ -99,6 +136,30 @@ export default {
   },
 
   methods: {
+    login(e) {
+      let _this = this;
+      $.post(
+        "https://localhost/api/v1/login",
+        $(e.target).serialize()
+      ).done(function(data) {
+        if (data.stt == 1) {
+          _this.msgLogin = data.msg;
+          localStorage.setItem("auth", 2);
+          localStorage.setItem("uid", data.uid);
+          location.reload();
+        } else {
+          _this.msgLogin = data.msg;
+          localStorage.setItem("auth", 1);
+          localStorage.setItem("uid", null);
+        }
+      });
+    },
+    logout() {
+      localStorage.removeItem("auth");
+      localStorage.removeItem("uid");
+      localStorage.removeItem("scanner");
+      location.reload();
+    },
     search() {
       window.location = `https://www.pinterest.com/search/${$(
         "#type"
@@ -113,9 +174,20 @@ export default {
       this.stop = 1;
     },
     sendLink() {
-      if (this.result.length > 0) {
-        console.log("send data success");
-      }
+      // if (this.result.length > 0) {
+      //   var jqxhr = $.get("/pinterest/link/follow/fetch", function() {
+      //     // alert("success");
+      //   })
+      //     .done(function(data) {
+      //       console.log(data);
+      //     })
+      //     .fail(function() {
+      //       console.log(error);
+      //     })
+      //     .always(function() {
+      //       console.log("finished");
+      //     });
+      // }
     }
   },
   created() {
